4ed04e2118972901ea5675593b025ef575453408
Sandesh Chapagain <sandesh@Sandeshs-MacBook-Pro.local>
2020-09-05 19:30:26 -0400

    initial commit

---
+++ CS50W_harvard/lecture3/newyear/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    from datetime import datetime
+    now = datetime.now()
+    return render(request,"newyear/index.html",{
+        "newyear": now.month ==1 and now.year == 1
+    })
