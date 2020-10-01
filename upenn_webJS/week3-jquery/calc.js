2a2f261956b29242ac2bfc0f3f3fa9d74a879195
Sandesh C <sandesh@greatbear.tech>
2020-10-01 14:18:50 -0400

    handle numeric button clicks

--- upenn_webJS/week3-jquery/calc.js
+++ upenn_webJS/week3-jquery/calc.js
@@ -1,3 +1,13 @@
 /*
  * Implement all your JavaScript in this file!
  */
+
+ $('.numeric-button').click( function(ev) {
+     var prevDisplay = $('#display').val();
+     var newDisplay = prevDisplay.concat($(this).val());
+     $('#display').val(newDisplay);
+ });
+
+ $('#clearButton').click( function(ev) {
+    $('#display').val('');
+ });
