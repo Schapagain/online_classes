5414ccfe4598babcccf8a0b09c2963ff4332f809
Sandesh C <sandesh@greatbear.tech>
2020-10-02 09:10:51 -0400

    refactor, implement numeric buttons and clear

--- upenn_webJS/week3-jquery/calc.js
+++ upenn_webJS/week3-jquery/calc.js
@@ -2,12 +2,34 @@
  * Implement all your JavaScript in this file!
  */
 
- $('.numeric-button').click( function(ev) {
-     var prevDisplay = $('#display').val();
-     var newDisplay = prevDisplay.concat($(this).val());
-     $('#display').val(newDisplay);
+var operationCodes = new Set([42,43,45,247]);
+
+ $('button').click( function(ev) {
+     
+    var currentButton = $(this).html();
+    if (currentButton === '=') {
+        handleEquals();
+    }else if(currentButton === "C") {
+        handleClear();
+    }else if(operationCodes.has(currentButton.charCodeAt(0))) {
+        handleOperation(currentButton);
+    }else {
+        handleNumber(currentButton);
+    }
  });
 
- $('#clearButton').click( function(ev) {
-    $('#display').val('');
- });
+ function handleClear(){
+    $('#display').val(''); 
+ }
+
+ function handleOperation(operation) {
+ }
+
+ function handleNumber(numberPressed){
+    var prevDisplay = $('#display').val();
+    var newDisplay = prevDisplay.concat(numberPressed);
+    $('#display').val(newDisplay);
+ }
+
+ function handleEquals(){
+ }
