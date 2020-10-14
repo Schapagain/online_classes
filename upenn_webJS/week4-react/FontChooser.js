064f27b72b09085752f9139a43f215f4cb51c4a2
Sandesh C <sandesh@greatbear.tech>
2020-10-14 10:48:39 -0400

    implement all required functionality

--- upenn_webJS/week4-react/FontChooser.js
+++ upenn_webJS/week4-react/FontChooser.js
@@ -1,20 +1,70 @@
 class FontChooser extends React.Component {
 
     constructor(props) {
-	super(props);
+		super(props);
+		this.state = {
+			bold: props.bold == 'true',
+			size: Number(props.size)<Number(props.min)? Math.max(Number(props.min),1):Number(props.size),
+			minSize: Math.max(Number(props.min),1),
+			maxSize: Math.max(Number(props.max),Number(props.min),1,Number(props.size)),
+			formHidden: true,
+			text: props.text,
+		}
+		this.baseSize = Number(props.size);
+		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+		this.handleTextClick = this.handleTextClick.bind(this);
+		this.handleFontDecrease = this.handleFontDecrease.bind(this);
+		this.handleFontIncrease = this.handleFontIncrease.bind(this);
+		this.handleFontReset = this.handleFontReset.bind(this);
     }
-    
+	
+	handleCheckboxChange(event) {
+		this.setState({
+			bold: !this.state.bold,
+		})
+	}
+
+	handleTextClick(event) {
+		this.setState({
+			formHidden: !this.state.formHidden,
+		})
+	}
+
+	handleFontDecrease (event) {
+		console.l
+		if (this.state.size > this.state.minSize){
+			this.setState({
+				size: this.state.size - 1
+			})
+		}
+	}
+
+	handleFontIncrease (event) {
+
+		if (this.state.size < this.state.maxSize){
+			this.setState({
+				size: this.state.size + 1
+			})
+		}
+	}
+
+	handleFontReset (event) {
+		this.setState({
+			size: this.baseSize
+		})
+	}
 
     render() {
 
 	return(
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-			<span id="textSpan" style={{fontSize: this.props.size+'px'}}>
-				{this.props.bold=='true'? <b>{this.props.text}</b>:this.props.text}
+		   <input onChange={this.handleCheckboxChange}
+		   checked={this.state.bold} type="checkbox" id="boldCheckbox" hidden={this.state.formHidden}/>
+	       <button id="decreaseButton" onClick={this.handleFontDecrease} hidden={this.state.formHidden}>-</button>
+	       <span id="fontSizeSpan" onDoubleClick={this.handleFontReset} style={{color: (this.state.size == this.state.maxSize || this.state.size == this.state.minSize)? 'red':'black'}} hidden={this.state.formHidden}>{this.state.size}</span>
+	       <button id="increaseButton" onClick={this.handleFontIncrease} hidden={this.state.formHidden}>+</button>
+			<span onClick={this.handleTextClick} id="textSpan" style={{fontSize: this.state.size,fontWeight: this.state.bold?'bold':'normal'}}>
+				{this.state.text}
 			</span>
 	       </div>
 	);
